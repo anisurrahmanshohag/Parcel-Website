375be7c8ae90e0dd097c3097998492ca84697d95
anisurrahmanshohag <76775918+anisurrahmanshohag@users.noreply.github.com>
2021-07-24 22:46:18 +0600

    First commit

---
+++ js/custom.js
@@ -0,0 +1,25 @@
+$(function() {
+    'use strict';
+
+    // banner_part start
+    $('.port1').on('click', function() {
+        $('.local').addClass('tactive'),
+            $('.global').addClass('pactive'),
+            $('.global').removeClass('tactive')
+    });
+    $('.port2').on('click', function() {
+        $('.global').addClass('tactive'),
+            $('.local').addClass('pactive'),
+            $('.local').removeClass('tactive')
+    });
+
+    $('.port1').on('click', function() {
+        $('.query').addClass('finding_box'),
+            $('.query').removeClass('shiping_box')
+    });
+    $('.port2').on('click', function() {
+        $('.query').addClass('shiping_box'),
+            $('.query').removeClass('finding_box')
+    });
+    // banner_part end
+})
